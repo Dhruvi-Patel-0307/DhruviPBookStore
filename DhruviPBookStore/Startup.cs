323c13acb590e77b1290ea6cd9ed2fbb377768a9
W0831844 <W0831844@ADL-A1201K-22>
2023-12-04 14:18:55 -0500

    add the Customers and modify the Startup sile and then move the Homecontroller

--- DhruviPBookStore/Startup.cs
+++ DhruviPBookStore/Startup.cs
@@ -63,7 +63,7 @@ namespace DhruviPBookStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customers}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
