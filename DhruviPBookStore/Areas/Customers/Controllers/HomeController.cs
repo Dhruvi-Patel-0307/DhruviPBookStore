12a9a56ca10b2f9fad69e507d58747e30c0f8f45
W0831844 <W0831844@ADL-A1201B-28>
2023-12-05 13:59:03 -0500

    part 4, updated homr index and controller.

--- DhruviPBookStore/Areas/Customers/Controllers/HomeController.cs
+++ DhruviPBookStore/Areas/Customers/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using DhruviPBookStore.Models;
+﻿using DhruviPBooks.DataAccess.Repository.IRepository;
+using DhruviPBooks.Models;
+using DhruviPBookStore.Models;
+using DhruviPBookStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,15 +16,18 @@ namespace DhruviPBookStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unifOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unifOfWork)
         {
             _logger = logger;
+            _unifOfWork = unifOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unifOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
@@ -32,7 +38,7 @@ namespace DhruviPBookStore.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new DhruviPBookStore.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
